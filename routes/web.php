34abcc40aeb6bb8eb592600e6c4a151e68cc498e
KyiPhyu <kyiphyu@eramyanmar.com>
2020-04-30 15:59:08 +0630

    create pages controller

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,12 @@
 use App\Article;
 use App\Http\Controllers\ArticlesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // Default render the view
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', 'PagesController@home');
 
 Route::get('/articles', function () {
     return view('articles.index', [

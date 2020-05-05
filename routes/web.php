5887bc14e50147c3772e19a34e76057d22019310
KyiPhyu <kyiphyu@eramyanmar.com>
2020-05-05 11:21:02 +0630

    Service Container Fundamentals

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,41 @@
 <?php
 
-
-use App\Article;
-use App\Http\Controllers\ArticlesController;
-
-// Default render the view
 Route::get('/', function () {
-    return view('welcome');
+
+    // $container = new \App\Container();
+    // $container->bind('example', function () {
+    //     return new \App\Example();
+    // });
+
+    // $example = $container->resolve('example');
+
+    // $example->go();
 });
 
-// Route::get('/', 'PagesController@home');
 
-Route::get('/articles', function () {
-    return view('articles.index', [
-        'articles' => App\Article::latest()->get()
-    ]);
-});
+// use App\Article;
+// use App\Http\Controllers\ArticlesController;
 
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');;
-Route::post('/articles', 'ArticlesController@store');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
-Route::get('/articles/{article}/edit', 'ArticlesController@edit');
-Route::put('/articles/{article}', 'ArticlesController@update');
+// // Default render the view
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-//contact mail
-Route::get('/contact', 'ContactController@show');
-Route::post('/contact', 'ContactController@store');
+// // Route::get('/', 'PagesController@home');
+
+// Route::get('/articles', function () {
+//     return view('articles.index', [
+//         'articles' => App\Article::latest()->get()
+//     ]);
+// });
+
+// Route::get('/articles', 'ArticlesController@index')->name('articles.index');;
+// Route::post('/articles', 'ArticlesController@store');
+// Route::get('/articles/create', 'ArticlesController@create');
+// Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
+// Route::get('/articles/{article}/edit', 'ArticlesController@edit');
+// Route::put('/articles/{article}', 'ArticlesController@update');
+
+// //contact mail
+// Route::get('/contact', 'ContactController@show');
+// Route::post('/contact', 'ContactController@store');
